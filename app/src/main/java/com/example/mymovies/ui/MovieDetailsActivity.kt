41c1b7cfd115d17a8102c2ccb2c18cfdf9424352
thomaskovoor <thomaskovoor123@gmail.com>
2024-02-27 18:53:34 +0530

    Remove dark mode,add MovieDetailsActivity

---
+++ app/src/main/java/com/example/mymovies/ui/MovieDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mymovies.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mymovies.R
+
+class MovieDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
---
+++ app/src/main/java/com/example/mymovies/ui/MyApplication.kt
@@ -0,0 +1,10 @@
+package com.example.mymovies.ui
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+    class MyApplication : Application() {
+        override fun onCreate() {
+            super.onCreate()
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
